f76120292b75f64d2388dd41a0a79887c2b8258f
18779106195 <>
2018-12-18 11:14:06 +0800

    first

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -15,11 +15,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func action1(_ sender: Any) {
+        let alert = UIAlertController(title: "alert", message: "login", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: { (action) in
+        }))
+        alert.addTextField { (textField) in
+            textField.placeholder = "name"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "password"
+            textField.isSecureTextEntry = true
+        }
+        present(alert, animated: true, completion: nil)
     }
-
+    
+    @IBAction func action3(_ sender: Any) {
+        let action = UIAlertController(title: "action_sheet", message: "please", preferredStyle: .actionSheet)
+        action.addAction(UIAlertAction(title: "red", style: .default, handler: { (action) in
+            self.view.backgroundColor = UIColor.red
+        }))
+        
+        action.addAction(UIAlertAction(title: "green", style: .destructive, handler: { (action) in
+            self.view.backgroundColor = UIColor.green
+        }))
+        
+        action.addAction(UIAlertAction(title: "black", style: .cancel, handler: { (action) in
+            self.view.backgroundColor = UIColor.black
+        }))
+        
+        present(action, animated: true, completion: nil)
+    }
+    
 
 }
 
